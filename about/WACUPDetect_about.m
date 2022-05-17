9f24575eb35d114da602aa05b6940313770c93b4
Eris Lund <38136789+0x5066@users.noreply.github.com>
2022-05-17 17:48:03 +0200

    extended the WACUPDetect script to now also change the main window in the about page

--- about/WACUPDetect_about.m
+++ about/WACUPDetect_about.m
@@ -4,6 +4,8 @@
 Global group frameGroup;
 Global Text WACUPtxt;
 
+Global layer wacupmain, wacuptitlebar;
+
 Function DetectWACUP();
 
 System.onScriptLoaded(){
@@ -13,9 +15,18 @@ System.onScriptLoaded(){
 
     WACUPtxt = frameGroup.findObject("isthiswacup");
 
+    wacupmain = frameGroup.findObject("mainwnd");
+	wacuptitlebar = frameGroup.findObject("wa.titlebar");
+
     if(IsWACUP == 1){
         WACUPtxt.setText("Is this WACUP?: Yes");
+        wacupmain.setXmlParam("image", "wacup.main");
+		wacuptitlebar.setXmlParam("image", "wacup.titlebar.on");
+		wacuptitlebar.setXmlParam("inactiveimage", "wacup.titlebar.off");
     }else if(IsWACUP == 0){
         WACUPtxt.setText("Is this WACUP?: No");
+        wacupmain.setXmlParam("image", "wa.main");
+		wacuptitlebar.setXmlParam("image", "wa.titlebar.on");
+		wacuptitlebar.setXmlParam("inactiveimage", "wa.titlebar.off");
     }
 }
